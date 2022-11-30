7dc280029044235f3f4cbbb6be7e3196f0070d8f
leo99effert <leo99effert@outlook.com>
2022-11-30 11:59:46 +0100

    Tested and refactored makeGoal()-method. I did it in another git repository where I accidently didnt include the test project so i had to move everything to this repository.

---
+++ Test/SecretSequenceTest.cs
@@ -0,0 +1,35 @@
+﻿using MooGame;
+namespace Test
+{
+    [TestClass]
+    public class SecretSequenceTest
+    {
+        [TestMethod]
+        public void OnlyNumbers()
+        {
+            Assert.IsTrue(MainClass.CreateSecretSequence().All(char.IsDigit));
+        }
+        [TestMethod]
+        public void LengthIsFour()
+        {
+            Assert.AreEqual(4, MainClass.CreateSecretSequence().Length);
+        }
+        [TestMethod]
+        public void AllCharsAreUnique()
+        {
+            string secretSequence = MainClass.CreateSecretSequence();
+            int uniqueChars = secretSequence.Length;
+            for (int i = 0; i < secretSequence.Length; i++)
+            {
+                for (int j = 0; j < secretSequence.Length; j++)
+                {
+                    if (secretSequence[i] == secretSequence[j] && i != j)
+                    {
+                        uniqueChars--;
+                    }
+                }
+            }
+            Assert.AreEqual(secretSequence.Length, uniqueChars);
+        }
+    }
+}
