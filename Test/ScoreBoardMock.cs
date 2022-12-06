6369f999391003eb3acc4b515bc232ea5c007364
leo99effert <leo99effert@outlook.com>
2022-12-06 14:12:26 +0100

    Finalized ScoreBoardMock

--- Test/ScoreBoardMock.cs
+++ Test/ScoreBoardMock.cs
@@ -11,7 +11,7 @@
                 string name = nameAndScore[0];
                 int guesses = Convert.ToInt32(nameAndScore[1]);
 
-                PlayerDataMock player = new PlayerDataMock(name, guesses);
+                PlayerDataMock player = new(name, guesses);
                 int position = results.IndexOf(player);
                 if (position < 0)
                 {
@@ -22,13 +22,12 @@
                     results[position].Update(guesses);
                 }
             }
-            results.Sort((p1, p2) => p1.Average().CompareTo(p2.Average()));
+            List<PlayerDataMock> inOrderResults = results.OrderBy(p => p.Average()).ToList();
             string printedResult = "";
             printedResult += "Player   games average\n";
-
-            foreach (PlayerDataMock player in results)
+            foreach (PlayerDataMock player in inOrderResults)
             {
-                printedResult += string.Format("{0,-9}{1,5:D}{2,9:F2}\n", player.Name, player.NGames, player.Average());
+                printedResult += string.Format("{0,-9}{1,5:D}{2,9:F2}\n", player.Name, player.GamesPlayed, player.Average());
             }
             return printedResult;
         }
@@ -36,28 +35,27 @@
     public class PlayerDataMock
     {
         public string Name { get; private set; }
-        public int NGames { get; private set; }
-        int totalGuess;
+        public int GamesPlayed { get; private set; }
+        public int TotalGuesses { get; set; }
 
 
         public PlayerDataMock(string name, int guesses)
         {
-            this.Name = name;
-            NGames = 1;
-            totalGuess = guesses;
+            Name = name;
+            GamesPlayed = 1;
+            TotalGuesses = guesses;
         }
 
         public void Update(int guesses)
         {
-            totalGuess += guesses;
-            NGames++;
+            TotalGuesses += guesses;
+            GamesPlayed++;
         }
 
         public double Average()
         {
-            return (double)totalGuess / NGames;
+            return (double)TotalGuesses / GamesPlayed;
         }
-
 
         public override bool Equals(Object p)
         {
--- Test/ScoreBoardMockTest.cs
+++ Test/ScoreBoardMockTest.cs
@@ -9,11 +9,15 @@
             "l3o#&#1",
             "leo99effert#&#16",
             "leo#&#1",
-            "peo#&#1"
+            "peo#&#1",
+            "neo#&#2",
+            "zlatan#&#1"
         };
         private string mockForPrintResult = "Player   games average\n" +
                                             "l3o          1     1,00\n" +
                                             "peo          1     1,00\n" +
+                                            "zlatan       1     1,00\n" +
+                                            "neo          1     2,00\n" +
                                             "leo          2     2,50\n" +
                                             "leo99effert    1    16,00\n";
         [TestMethod]
