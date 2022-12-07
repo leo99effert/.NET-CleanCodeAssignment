3d6ea0943669d2389a3e63603d9cc0b12edf50d2
leo99effert <leo99effert@outlook.com>
2022-12-07 15:51:53 +0100

    Removed code that was written just before it was written again inside a while loop

--- Moo/Program.cs
+++ Moo/Program.cs
@@ -14,23 +14,18 @@ namespace MooGame
             while (continuePlaying)
             {
                 string secretSequence = GameCalculator.CreateSecretSequence();
-
-
                 Console.WriteLine("New game:\n");
                 //comment out or remove next line to play real games!
                 Console.WriteLine("For practice, number is: " + secretSequence + "\n");
-                string guess = Console.ReadLine();
 
-                playerData.TotalGuesses++;
-                string bullsAndCows = GameCalculator.GetBullsAndCows(secretSequence, guess);
-                Console.WriteLine(bullsAndCows + "\n");
-                while (bullsAndCows != "BBBB,")
+                string currentBullsAndCows = ",";
+                while (currentBullsAndCows != "BBBB,")
                 {
-                    guess = Console.ReadLine();
+                    string guess = Console.ReadLine();
                     playerData.TotalGuesses++;
                     Console.WriteLine(guess + "\n");
-                    bullsAndCows = GameCalculator.GetBullsAndCows(secretSequence, guess);
-                    Console.WriteLine(bullsAndCows + "\n");
+                    currentBullsAndCows = GameCalculator.GetBullsAndCows(secretSequence, guess);
+                    Console.WriteLine(currentBullsAndCows + "\n");
                 }
 
                 Console.WriteLine("Correct, it took " + playerData.TotalGuesses + " guesses\n");
