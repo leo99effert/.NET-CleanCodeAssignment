aecd82f087a8e619a3982cca501596aaa78ac8a6
leo99effert <leo99effert@outlook.com>
2022-12-08 10:02:35 +0100

    Removed unnesisary code in program.cs

--- Moo/Program.cs
+++ Moo/Program.cs
@@ -1,26 +1,17 @@
 ﻿using Moo;
 
-namespace MooGame
+Game game = new();
+game.CreatePlayer();
+
+bool continuePlaying = true;
+while (continuePlaying)
 {
-    public class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Game game = new();
-            game.CreatePlayer();
+    string secretSequence = GameCalculator.CreateSecretSequence();
+    Console.WriteLine("New game:\n");
+    //comment out or remove next line to play real games!
+    Console.WriteLine("For practice, number is: " + secretSequence + "\n");
 
-            bool continuePlaying = true;
-            while (continuePlaying)
-            {
-                string secretSequence = GameCalculator.CreateSecretSequence();
-                Console.WriteLine("New game:\n");
-                //comment out or remove next line to play real games!
-                Console.WriteLine("For practice, number is: " + secretSequence + "\n");
-
-                game.RunGameLoop(secretSequence);
-                game.EndAndSave();
-                continuePlaying = game.AskForNewGame();
-            }
-        }
-    }
+    game.RunGameLoop(secretSequence);
+    game.EndAndSave();
+    continuePlaying = game.AskForNewGame();
 }
