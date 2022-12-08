f78589647f2c185a3f2abc1ecdb5b9ce795b81bf
leo99effert <leo99effert@outlook.com>
2022-12-08 09:56:49 +0100

    Created Game Class

---
+++ Moo/Game.cs
@@ -0,0 +1,43 @@
+﻿namespace Moo
+{
+    public class Game
+    {
+        public PlayerData Player { get; set; }
+        public void CreatePlayer()
+        {
+            Console.WriteLine("Enter your user name:\n");
+            Player = new(Console.ReadLine());
+        }
+        public void RunGameLoop(string secretSequence)
+        {
+            string currentBullsAndCows = ",";
+            Player.TotalGuesses = 0;
+            while (currentBullsAndCows != "BBBB,")
+            {
+                string guess = Console.ReadLine();
+                Player.TotalGuesses++;
+                Console.WriteLine(guess + "\n");
+                currentBullsAndCows = GameCalculator.GetBullsAndCows(secretSequence, guess);
+                Console.WriteLine(currentBullsAndCows + "\n");
+            }
+        }
+        public void EndAndSave()
+        {
+            Console.WriteLine("Correct, it took " + Player.TotalGuesses + " guesses\n");
+            HighScoresHandler.WriteToTextFile(Player);
+            List<string> dataEntries = HighScoresHandler.ReadTextFile();
+            List<PlayerData> playerDatas = HighScoresHandler.ConvertToPlayerData(dataEntries);
+            Console.WriteLine(HighScoresHandler.CreateConsoleString(playerDatas));
+        }
+        public bool AskForNewGame()
+        {
+            Console.WriteLine("Continue?");
+            string answer = Console.ReadLine();
+            if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Moo/Program.cs
+++ Moo/Program.cs
@@ -6,9 +6,8 @@ namespace MooGame
     {
         public static void Main(string[] args)
         {
-
-            Console.WriteLine("Enter your user name:\n");
-            PlayerData playerData = new(Console.ReadLine());
+            Game game = new();
+            game.CreatePlayer();
 
             bool continuePlaying = true;
             while (continuePlaying)
@@ -18,28 +17,9 @@ namespace MooGame
                 //comment out or remove next line to play real games!
                 Console.WriteLine("For practice, number is: " + secretSequence + "\n");
 
-                string currentBullsAndCows = ",";
-                while (currentBullsAndCows != "BBBB,")
-                {
-                    string guess = Console.ReadLine();
-                    playerData.TotalGuesses++;
-                    Console.WriteLine(guess + "\n");
-                    currentBullsAndCows = GameCalculator.GetBullsAndCows(secretSequence, guess);
-                    Console.WriteLine(currentBullsAndCows + "\n");
-                }
-
-                Console.WriteLine("Correct, it took " + playerData.TotalGuesses + " guesses\n");
-                HighScoresHandler.WriteToTextFile(playerData);
-                List<string> dataEntries = HighScoresHandler.ReadTextFile();
-                List<PlayerData> playerDatas = HighScoresHandler.ConvertToPlayerData(dataEntries);
-                Console.WriteLine(HighScoresHandler.CreateConsoleString(playerDatas));
-
-                Console.WriteLine("Continue?");
-                string answer = Console.ReadLine();
-                if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
-                {
-                    continuePlaying = false;
-                }
+                game.RunGameLoop(secretSequence);
+                game.EndAndSave();
+                continuePlaying = game.AskForNewGame();
             }
         }
     }
